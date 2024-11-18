2830588dfc544ebf939fdb07714092cd8d654eb0
AsifurRahman10 <1720105@iub.edu.bd>
2024-11-18 12:45:49 +0600

    Basic project structure setup

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDTIUquyxV8ulQ-HYM7I5OXm6qEAl4yBY",
+  authDomain: "lingo-bingo-wesbite.firebaseapp.com",
+  projectId: "lingo-bingo-wesbite",
+  storageBucket: "lingo-bingo-wesbite.firebasestorage.app",
+  messagingSenderId: "1044086510444",
+  appId: "1:1044086510444:web:ec7298f66feb5ba0521949"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
